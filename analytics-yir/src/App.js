4133dcd14de53cf8eaa65706949a16746cb1452a
Cindy Zhu <zhucindy1234567890@gmail.com>
2021-03-20 17:02:02 -0400

    finished bAndI page - images still not showing

--- analytics-yir/src/App.js
+++ analytics-yir/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //import Example from './containers/example-base-page';
 import JournalismNav from './containers/journalism_nav';
+import BAndINav from './containers/bAndI_nav';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
         <Switch>
-          <Route exact path='/' component={JournalismNav} />
+          <Route exact path='/' component={BAndINav} />
         </Switch>
       </Router>
   );
---
+++ analytics-yir/src/data/bAndI_nav_tiles.js
@@ -0,0 +1,33 @@
+export const bAndI_nav_tiles = [
+    {
+        section: "revenue",
+        link: "https://www.google.com/",
+        img: 'revenue.png'
+    },
+    {
+        section: "sbs",
+        link: "https://www.google.com/",
+        img: 'sbs.png'
+    },
+    {
+        section: "engagement",
+        link: "https://www.google.com/",
+        img: 'engagement.png'
+    },
+    {
+        section: "product",
+        link: "https://www.google.com/",
+        img: 'product.png'
+    },
+    {
+        section: "engineering",
+        link: "https://www.google.com/",
+        img: 'engineering.png'
+    },
+    {
+        section: "spectrum",
+        link: "https://www.google.com/",
+        img: 'spectrum_bAndI.png'
+    } 
+];
+
